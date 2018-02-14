a52243022d0c0605c39a3fa1638ea2459fe5351d
ShinJongMin <standbyme227@gmail.com>
2018-02-14 10:22:35 +0900

    웹툰 list를 가져올 함수를 만든후, crawling수업 영상을 보면서. os.path에 대해 공부한뒤. root폴더 안에 data폴더를 만들고 (겹치면 그냥 넘어가게), 그 data폴더 안에 넣을 파일의 경로를 os.path로 설정한 뒤. try-except와 if문 두가지 방식을 이용해서 파일이 존재하면 에러메세지가 뜨고. 아니면 생성하도록 설정

---
+++ homework_1/main.py
@@ -0,0 +1,13 @@
+import re
+from bs4 import BeautifulSoup
+from utils import get_week_list
+
+# source = open('melon.html', 'rt').read()
+# soup = BeautifulSoup(source, 'lxml')
+
+
+
+if __name__ == '__main__':
+    result = get_week_list()
+    # for item in result:
+    #     print(item)
